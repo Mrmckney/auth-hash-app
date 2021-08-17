0fe3500e56890c86e4298c821a0442ca822773d1
Matthew McKney <mrmckney@gmail.com>
2021-08-17 10:58:39 -0400

    added protected form

---
+++ src/components/Login.jsx
@@ -0,0 +1,67 @@
+import bcrypt from 'bcryptjs'
+import { Form, Input, Button } from 'antd'
+
+const mySalt = "$2b$10$ibXdpDiUvdVne89h2QdtKe"
+
+function Login({setUser, setReturningUser}) {
+
+  function handleLogin({email, password}) {
+    const hashedPassword = bcrypt.hashSync(password, mySalt)
+    fetch('https://auth-hash-api-mm.web.app/login', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ email, password: hashedPassword })
+    })
+      .then(response => response.json())
+      .then(data => {
+          setUser(data.user || { email })
+      })
+      .catch(err => alert(err))
+  }
+
+  return (
+    <Form
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      onFinish={handleLogin}
+    >
+      <h1>Login</h1>
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, message: 'Please input your email!' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
+        <Button 
+          type="primary" 
+          htmlType="submit"
+        >
+            Login
+        </Button>
+        &nbsp;
+        <Button 
+          type="ghost" 
+          htmlType="button"
+          onClick={() => setReturningUser(false)}
+        >
+            Go to Signup
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default Login
